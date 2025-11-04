fa88456b5d3be0ccb6c1f02db50e7bd6eca5b308
salonimaheshwarii <slonimaheshwari@gmail.com>
2025-11-05 00:13:36 +0530

    Trigger deploy workflow

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true, // Enables React's Strict Mode
+  reactStrictMode: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: 'SALONIMAHESHWARI',
+
 };
 
 export default nextConfig;
